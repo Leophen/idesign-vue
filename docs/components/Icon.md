7b219b73f046a4dc38b0c4b490e7f1d351cd670b
Leophen <Leophen@foxmail.com>
2022-07-19 19:59:11 +0800

    feat: Icon components

--- docs/components/Icon.md
+++ docs/components/Icon.md
@@ -1,4 +1,4 @@
-# Icon 图标（开发中）
+# Icon 图标
 
 iDesign 提供了一套常用的图标集合，可直接使用。
 
@@ -27,3 +27,80 @@ icon/demo3
 :::
 
 ## 全部图标
+
+<ul class="i-design-demo-icon-list">
+  <li
+    v-for="item in sortedIconArr"
+    :class="getCls(item.name)"
+    :key="item.icon_id"
+    @click="handleCopyName(item.name)"
+  >
+    <i-icon :name="item.name" :size="32" />
+    <div>{{ item.name }}</div>
+  </li>
+</ul>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue'
+import axios from 'axios'
+import _ from 'lodash'
+
+export default defineComponent({
+  setup() {
+    const api =
+      'https://at.alicdn.com/t/font_3161433_glke53nnqws.json?spm=a313x.7781069.1998910419.80&file=font_3161433_glke53nnqws.json'
+
+    const sortedIconArr: any = ref(null)
+
+    onMounted(async () => {
+      const data = await axios.get(api).then((res) => {
+        return _.sortBy(res.data.glyphs, (item) => item.name)
+      })
+      sortedIconArr.value = data
+    })
+
+    const copyed = ref(false)
+    const curName = ref('')
+
+    const handleCopyName = (name) => {
+      navigator.clipboard.writeText(`<i-icon name="${name}" />`)
+      copyed.value = true
+      curName.value = name
+      setTimeout(resetCopyTip, 500)
+    }
+
+    const resetCopyTip = () => {
+      copyed.value = false
+      curName.value = ''
+      // @ts-ignore
+      clearTimeout(resetCopyTip)
+    }
+
+    const getCls = (itemName) => {
+      return {
+        'i-design-demo-icon-item': true,
+        '-copyed': curName.value === itemName && copyed.value
+      }
+    }
+
+    return {
+      curName,
+      sortedIconArr,
+      copyed,
+      handleCopyName,
+      getCls
+    }
+  }
+})
+</script>
+
+
+## BreadcrumbItem API
+
+| 属性     | 说明             | 类型                       | 默认值  |
+| -------- | ---------------- | -------------------------- | ------- |
+| name     | 图标名称         | `string`                   | `--`    |
+| size     | 图标尺寸         | `string〡number`           | `16`    |
+| color    | 图标颜色         | `string`                   | `--`    |
+| disabled | 是否禁用图标     | `boolean`                  | `false` |
+| @click   | 点击图标触发事件 | `(ev: MouseEvent) => true` | `--`    |
--- docs/demo/icon/demo4.vue
+++ docs/demo/icon/demo4.vue
@@ -1,92 +1,58 @@
 <template>
-  <ul className="i-design-demo-icon-list">
+  <ul class="i-design-demo-icon-list">
     <li
       v-for="item in sortedIconArr"
       :class="getCls(item.name)"
       :key="item.icon_id"
       @click="handleCopyName(item.name)"
     >
-      <Icon :name="item.name" :size="32" />
+      <i-icon :name="item.name" :size="32" />
       <div>{{ item.name }}</div>
     </li>
   </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, computed } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
 import axios from 'axios'
 import _ from 'lodash'
 
 export default defineComponent({
-  name: 'Icon',
-  props: {
-    /**
-     * 图标名称
-     */
-    name: {
-      type: String,
-      default: 'UserAvatar'
-    },
-    /**
-     * 图标名称
-     */
-    size: [Number, String],
-    /**
-     * 图标颜色
-     */
-    color: String,
-    /**
-     * 是否禁用按钮
-     */
-    disabled: {
-      type: Boolean,
-      default: false
-    }
-  },
-  emits: {
-    /**
-     * @zh 点击按钮时触发
-     * @en Emitted when the button is clicked
-     * @property {MouseEvent} ev
-     */
-    click: (ev: MouseEvent) => true
-  },
   setup() {
     const api =
       'https://at.alicdn.com/t/font_3161433_glke53nnqws.json?spm=a313x.7781069.1998910419.80&file=font_3161433_glke53nnqws.json'
 
-    let sortedIconArr: any = []
+    const sortedIconArr: any = ref(null)
 
-    onMounted(() => {
-      axios
-        .get(api)
-        .then((res) => {
-          sortedIconArr = _.sortBy(res.data.glyphs, (item) => item.name)
-        })
-        .finally(() => {})
+    onMounted(async () => {
+      const data = await axios.get(api).then((res) => {
+        return _.sortBy(res.data.glyphs, (item) => item.name)
+      })
+      sortedIconArr.value = data
     })
 
     const copyed = ref(false)
     const curName = ref('')
 
-    const handleCopyName = (value) => {
-      navigator.clipboard.writeText(`<Icon name="${value}" />`)
+    const handleCopyName = (name) => {
+      navigator.clipboard.writeText(`<i-icon name="${name}" />`)
       copyed.value = true
-      curName.value = value
+      curName.value = name
       setTimeout(resetCopyTip, 500)
     }
 
     const resetCopyTip = () => {
       copyed.value = false
       curName.value = ''
-      // clearTimeout(resetCopyTip)
+      // @ts-ignore
+      clearTimeout(resetCopyTip)
     }
 
     const getCls = (itemName) => {
-      return computed(() => [
-        'i-design-demo-icon-item',
-        curName === itemName && copyed && '-copyed'
-      ])
+      return {
+        'i-design-demo-icon-item': true,
+        '-copyed': curName.value === itemName && copyed.value
+      }
     }
 
     return {
