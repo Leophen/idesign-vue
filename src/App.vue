a4dd0a9b0aedd038020fd3444262d3207d7fdd7c
Leophen <Leophen@foxmail.com>
2022-07-21 11:28:27 +0800

    fix: build error

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <template>
-  123
+  <i-grid :gutter="10">
+    <i-grid-item :span="2">
+      <div>1</div>
+    </i-grid-item>
+    <i-grid-item :span="2">
+      <div>2</div>
+    </i-grid-item>
+    <i-grid-item :span="2" :order="4">
+      <div>3</div>
+    </i-grid-item>
+    <i-grid-item :span="2" :order="3">
+      <div>4</div>
+    </i-grid-item>
+    <i-grid-item :span="2" :order="3">
+      <div>5</div>
+    </i-grid-item>
+    <i-grid-item :span="2" :order="2">
+      <div>6</div>
+    </i-grid-item>
+    <i-grid-item :span="2" :order="1">
+      <div>7</div>
+    </i-grid-item>
+    <i-grid-item :span="2" :order="0">
+      <div>8</div>
+    </i-grid-item>
+    <i-grid-item :span="2">
+      <div>9</div>
+    </i-grid-item>
+    <i-grid-item :span="2">
+      <div>10</div>
+    </i-grid-item>
+  </i-grid>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss">
 @import url(../vuepress/docs/.vuepress/styles/index.scss);
