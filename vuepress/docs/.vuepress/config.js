d8974572631a526cb8dae71fcbf003b26d381ce1
Leophen <Leophen@foxmail.com>
2022-08-20 16:24:41 +0800

    fix: ES bug

--- vuepress/docs/.vuepress/config.js
+++ vuepress/docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const { defaultTheme } = require("@vuepress/theme-default");
 const { viteBundler } = require("@vuepress/bundler-vite");
 const { docsearchPlugin } = require('@vuepress/plugin-docsearch')
 const { rightAnchorPlugin } = require('vuepress-plugin-right-anchor')
-const { sitemapPlugin } = require("vuepress-plugin-sitemap2")
+import { sitemapPlugin } from "vuepress-plugin-sitemap2";
 
 module.exports = {
   title: 'iDesign',
