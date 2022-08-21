7b9aa056b209ab51def40ac811afcdfa574a74ac
Leophen <leophen@foxmail.com>
2022-08-21 15:47:11 +0800

    Revert "refactor: 迁移主页组件"
    This reverts commit e4b18c40f0792958704bc3e29427a6dd78cdd8f2.

--- vuepress/docs/.vuepress/config.js
+++ vuepress/docs/.vuepress/config.js
@@ -4,12 +4,6 @@ const { viteBundler } = require("@vuepress/bundler-vite");
 const { docsearchPlugin } = require('@vuepress/plugin-docsearch')
 const { rightAnchorPlugin } = require('vuepress-plugin-right-anchor')
 const { sitemapPlugin } = require('vuepress-plugin-sitemap2')
-const {
-  registerComponentsPlugin
-} = require('@vuepress/plugin-register-components')
-const {
-  path
-} = require('@vuepress/utils')
 
 module.exports = {
   title: 'iDesign',
@@ -139,11 +133,6 @@ module.exports = {
     }),
     sitemapPlugin({
       hostname: 'https://idesign-vue.vercel.app/'
-    }),
-    registerComponentsPlugin({
-      components: {
-        Home: path.resolve(__dirname, './components/Home.vue'),
-      },
     }),
   ],
   markdown: {
